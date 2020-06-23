d215ec9828f0bc915160551b53c1a3aa49356500
Flora Petersen <floramarina@comcast.net>
2020-06-23 17:18:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     @total
   end
 
-  def add_item(item, price, quantity=1)
+  def add_item(item, price, quantity = 1)
     @total += price * quantity
     @items << item
     if quantity > 1
