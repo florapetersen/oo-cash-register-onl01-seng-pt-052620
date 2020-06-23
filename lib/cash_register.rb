c8188a2f3ee0098cf9b5e66373b72f7458f6a9a6
Flora Petersen <floramarina@comcast.net>
2020-06-23 17:11:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+  attr_accessor :total, :discount
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity=1)
+    @total += price * quantity
+  end
+
+  def apply_discount
+    if @discount > 0
+      discount_in_dollars = @total * discount / 100
+      @total = @total - discount_in_dollars
+      @total
+      "After the discount, the total comes to $#{total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+
+end
